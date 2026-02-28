96409b9e71f244efb5a88fc12700b219630fab07
ZJZXBQ <3095509806@qq.com>
2026-02-28 15:52:26 +0800

    second commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.litepal.core)
+    implementation(libs.okhttp)
+    implementation(libs.gson)
+    implementation(libs.glide)
 }
